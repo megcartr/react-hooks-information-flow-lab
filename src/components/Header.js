e4733041be42ef22d2a680b5f9c311fb1f7c3978
Meghan Carter <msc2010@yahoo.com>
2023-08-20 09:36:04 -0500

    lab completed

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header({onDarkModeClick, isDarkMode}) {
+    return(
+        <header>
+            <button onClick={() => onDarkModeClick()}>
+                {isDarkMode ? "Dark" : "Light"} Mode
+            </button>
+        </header>
+    )
+}
+
+export default Header;
